f6e0478691d7dd21029372a4f7d9dcfd505f7494
clairelee0817 <claire.y.lee0817@gmail.com>
2024-12-09 10:21:50 -0800

    api_claire

---
+++ api/api_claire.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask
+
+# add an api endpoint to flask app
+@app.route('/api/data')
+def get_data():
+    # start a list, to be used like a information database
+    InfoDb = []
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "John",
+        "LastName": "Mortensen",
+        "DOB": "October 21",
+        "Residence": "San Diego",
+        "Email": "jmortensen@powayusd.com",
+        "Owns_Cars": ["2015-Fusion", "2011-Ranger", "2003-Excursion", "1997-F350", "1969-Cadillac"]
+    })
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "Shane",
+        "LastName": "Lopez",
+        "DOB": "February 27",
+        "Residence": "San Diego",
+        "Email": "slopez@powayusd.com",
+        "Owns_Cars": ["2021-Insight"]
+    })
+    
+    return jsonify(InfoDb)
+
+# add an HTML endpoint to flask app
+@app.route('/')
+def say_hello():
+    html_content = """
+    <html>
+    <head>
+        <title>Hellox</title>
+    </head>
+    <body>
+        <h2>Hello, World!</h2>
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == '__main__':
+    # starts flask server on default port, http://127.0.0.1:5001
+    app.run(port=5001)
