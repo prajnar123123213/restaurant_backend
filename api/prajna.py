f13aca86dc494e33b28404792588956d56964be0
prajnar123123213 <prajnar4000@gmail.com>
2024-12-10 00:10:02 -0800

    added about me data

---
+++ api/prajna.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask
+
+# add an api endpoint to flask app
+@app.route('/api/data')
+def get_data():
+    # start a list, to be used like a information database
+    InfoDb = []
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "Prajna",
+        "LastName": "Raj",
+        "DOB": "May 24",
+        "Role": "Assitant Scrum",
+        "Residence": "San Diego",
+        "Email": "jmortensen@powayusd.com",
+        "Owns_Cars": ["2015-Fusion", "2011-Ranger", "2003-Excursion", "1997-F350", "1969-Cadillac"]
+    })
+    
+    return jsonify(InfoDb)
+
+# add an HTML endpoint to flask app
+@app.route('/')
+def say_hello():
+    html_content = """
+    <html>
+    <head>
+        <title>Hellox</title>
+    </head>
+    <body>
+        <h2>Hello, World!</h2>
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == '__main__':
+    # starts flask server on default port, http://127.0.0.1:5001
+    app.run(port=5001)
