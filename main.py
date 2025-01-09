4abe813d79d8bb95803ce84f174a58cec8df1e2d
prajnar123123213 <prajnar4000@gmail.com>
2025-01-08 19:38:02 -0800

    register nigeria

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from api.carChat import car_chat_api
 from api.student import student_api
 from api.africa import africa_api
 from api.ethiopia import ethiopia_api
+from api.nigeria import nigeria_api
 
 from api.vote import vote_api
 # database Initialization functions
@@ -52,6 +53,7 @@ app.register_blueprint(car_chat_api)
 app.register_blueprint(student_api)
 app.register_blueprint(africa_api)
 app.register_blueprint(ethiopia_api)
+app.register_blueprint(nigeria_api)
 
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
