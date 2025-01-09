70088257d10df18ce72c7f334cd7ea0a1ca1f369
zoeqinhe <zoeqinhe@gmail.com>
2025-01-08 21:49:33 -0800

    created china.py for china restaurants

---
+++ api/china.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, jsonify
+from flask_restful import Api, Resource
+
+china_api = Blueprint('china_api', __name__, url_prefix='/api')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(china_api)
+
+class ChinaAPI:
+    @staticmethod
+    def get_food(name):
+        food = {
+            "West_Lake_Restaurant": {
+                "name": "West_Lake_Restaurant",
+                "location": "China",
+            },
+            "Silk_Road_Cuisine": {
+                "name": "Silk_Road_Cuisine",
+                "location": "China",
+            },              
+        }
+        return food.get(name)
+    
+    class _West_Lake_Restaurant(Resource):
+        def get(self):
+            # Use the helper method to get Jeff's details
+            West_Lake_Restaurant_details = ChinaAPI.get_food("West_Lake_Restaurant")
+            return jsonify(West_Lake_Restaurant_details)
+        
+    class _Silk_Road_Cuisine(Resource):
+        def get(self):
+            # Use the helper method to get Jeff's details
+            Silk_Road_Cuisine_details = ChinaAPI.get_food("Silk_Road_Cuisine")
+            return jsonify(Silk_Road_Cuisine_details)
+        
+    class _Bulk(Resource):
+        def get(self):
+            # Use the helper method to get both John's and Jeff's details
+            West_Lake_Restaurant_details = ChinaAPI.get_food("West_Lake_Restaurant")
+            Silk_Road_Cuisine_details = ChinaAPI.get_food("Silk_Road_Cuisine_details")
+            return jsonify({"food": [West_Lake_Restaurant_details, Silk_Road_Cuisine_details]})
+
+    # Building REST API endpoints
+    api.add_resource(_West_Lake_Restaurant, '/food/west_lake_restaurant')
+    api.add_resource(_Silk_Road_Cuisine, '/food/silk_road_cuisine')
+    api.add_resource(_Bulk, '/food')
+
+# Instantiate the StudentAPI to register the endpoints
+china_api_instance = ChinaAPI()
