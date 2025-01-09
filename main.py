6e644e9ce923ebcae4786e6940b8eb8cbf2c216a
clairelee0817 <claire.y.lee0817@gmail.com>
2025-01-08 23:32:57 -0800

    brazil endpoint

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from api.nigeria import nigeria_api
 from api.china import china_api
 from api.japan import japan_api
 from api.india import india_api
+from api.brazil import brazil_api
 
 from api.vote import vote_api
 # database Initialization functions
@@ -60,6 +61,7 @@ app.register_blueprint(nigeria_api)
 app.register_blueprint(china_api)
 app.register_blueprint(japan_api)
 app.register_blueprint(india_api)
+app.register_blueprint(brazil_api)
 
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
